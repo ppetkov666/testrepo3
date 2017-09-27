3c147722114011dc62c820d90645a5f4288e03db
petko <ptpetkov666@gmail.com>
2017-09-27 14:54:43 -0700

    commiting testVB homework

---
+++ hello soft uni/hello soft uni/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_soft_uni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello softuni");
+        }
+    }
+}
